7222975e2e7955891ad31f691f6ec06902c6d77f
Hai Le Trung <letrunghai.92nl@gmail.com>
2023-12-15 21:51:47 +0700

    Update script

--- scripts/install_ffmpeg.sh
+++ scripts/install_ffmpeg.sh
@@ -5,9 +5,9 @@ cd /usr/local/bin
 mkdir ffmpeg
 
 cd ffmpeg
-wget https://www.johnvansickle.com/ffmpeg/old-releases/ffmpeg-4.2.1-amd64-static.tar.xz
-tar -xvf ffmpeg-4.2.1-amd64-static.tar.xz
-mv ffmpeg-4.2.1-amd64-static/ffmpeg .
+wget https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz
+tar -xvf ffmpeg-release-amd64-static.tar.xz
+mv ffmpeg-release-amd64-static/ffmpeg .
 
 ln -s /usr/local/bin/ffmpeg/ffmpeg /usr/bin/ffmpeg
 
