e979a66f1ee7f4aa1c01ae054a5cf947c3d323f3
Hai Le Trung <letrunghai.92nl@gmail.com>
2023-12-15 17:52:19 +0700

    Added script install ffmpeg on AMI Linux 2

---
+++ scripts/install_ffmpeg.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+sudo su -
+
+cd /usr/local/bin
+mkdir ffmpeg
+
+cd ffmpeg
+wget https://www.johnvansickle.com/ffmpeg/old-releases/ffmpeg-4.2.1-amd64-static.tar.xz
+tar xvf ffmpeg-4.2.1-amd64-static.tar.xz
+mv ffmpeg-4.2.1-amd64-static/ffmpeg .
+
+ln -s /usr/local/bin/ffmpeg/ffmpeg /usr/bin/ffmpeg
