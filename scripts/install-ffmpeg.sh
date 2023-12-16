8cfeb7d58d6d6c9055a15b5fc69c47e1a268da69
Hai Le Trung <letrunghai.92nl@gmail.com>
2023-12-16 14:40:27 +0700

    Update 16122023

--- scripts/install-ffmpeg.sh
+++ scripts/install-ffmpeg.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-yum update -y && yum install -y tar xz
-
 # Create a directory for FFmpeg
 mkdir -p $PWD/bin
 
 # Download FFmpeg
-curl -L https://ffmpeg.org/releases/ffmpeg-6.1.tar.xz | tar xz -C $PWD/bin --strip-components=1
+curl -L https://ffmpeg.org/releases/ffmpeg-6.1.tar.xz | tar xvf -C $PWD/bin --strip-components=1
 
 # Make binaries executable
 chmod +x $PWD/bin/*
