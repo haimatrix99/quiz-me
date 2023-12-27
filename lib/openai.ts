4e0fff3b25a6b20e542c54930967f304a326a2af
Hai Le Trung <letrunghai.92nl@gmail.com>
2023-12-27 16:42:49 +0700

    Update 27122023

--- lib/openai.ts
+++ lib/openai.ts
@@ -1,8 +1,10 @@
 import { ChatOpenAI } from "langchain/chat_models/openai";
 import { ChatPromptTemplate } from "langchain/prompts";
 
+
+
 const template = `You are a helpful assistant who generates questions with multiple choices with one correct answer. 
-A user will pass in a paragraph, and you should generate 5 questions with multiple choices with one correct answer and the correct answer of the question. The answers is following with format ["A.<Answer A>", "B.<Answer B>", "C.<Answer C>", "D.<Answer D>"]. If the question has two correct answer, the two correct answer will separator by "and". The output will be have JSON string and question stores in the list of the key of questions. The key of question is question, the key of the answers is answers, the key of correct answer is correct_answer.
+A user will pass in a paragraph, and you should generate 5 questions with multiple choices with one correct answer and the correct answer of the question. The answers is following with format ["A. <Answer A>", "B. <Answer B>", "C. <Answer C>", "D. <Answer D>"]. If the question has two correct answer, the two correct answer will separator by "and". The output will be have JSON string and question stores in the list of the key of questions. The key of question is question, the key of the answers is answers, the key of correct answer is correct_answer.
 ONLY return JSON string, and nothing more.`;
 
 const humanTemplate = "{transcript}";
