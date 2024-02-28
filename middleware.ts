0e6ecf3d480a1d52c962b48f91a772ddb61e9fd3
Le Trung Hai <hai.le@navigosgroup.com>
2024-02-28 12:54:15 +0700

    Handle CORS

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,5 @@
 import { authMiddleware } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
 
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
@@ -28,3 +29,19 @@ export const config = {
     "/sign-up",
   ],
 };
+
+export function middleware() {
+  // retrieve the current response
+  const res = NextResponse.next()
+
+  // add the CORS headers to the response
+  res.headers.append('Access-Control-Allow-Credentials', "true")
+  res.headers.append('Access-Control-Allow-Origin', '*') // replace this your actual origin
+  res.headers.append('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
+  res.headers.append(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+
+  return res
+}
