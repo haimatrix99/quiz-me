281ee244170bde56ec2e0840de83c35bd3408b8e
Hai Le Trung <letrunghai.92nl@gmail.com>
2024-02-28 23:59:21 +0700

    Update 28/02/2024 3

--- next.config.js
+++ next.config.js
@@ -18,20 +18,9 @@ const nextConfig = {
             },
         ],
     },
-    async headers() {
-        return [
-            {
-                // matching all API routes
-                source: "/(.*)",
-                headers: [
-                    { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
-                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-                ]
-            }
-        ]
-    }
+    headers: {
+        'Access-Control-Allow-Origin': '*',
+    },
 }
 
 module.exports = nextConfig
